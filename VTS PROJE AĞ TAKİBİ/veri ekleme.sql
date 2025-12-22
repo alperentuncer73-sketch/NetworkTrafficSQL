7477bbe5b21f3fd2db77d3bec5fa44e7fab3d441
Umut Can Kuş <umuut2024@gmail.com>
2025-12-22 22:40:49 +0300

    Delete VTS PROJE AĞ TAKİBİ/veri ekleme.sql

--- VTS PROJE AĞ TAKİBİ/veri ekleme.sql
+++
@@ -1,6 +0,0 @@
-USE [Ag takibi proje];
-GO
-INSERT INTO TrafficProtocols (ProtocolName, DefaultPort) VALUES ('TCP', NULL), ('UDP', NULL), ('HTTP', 80), ('HTTPS', 443), ('DNS', 53);
-INSERT INTO DeviceTypes (TypeName) VALUES ('Router'), ('Switch'), ('Modem'), ('Firewall');
-INSERT INTO DeviceModels (Manufacturer, ModelName) VALUES ('Cisco', '2900 Series'), ('HP', 'Aruba 3810M'), ('D-Link', 'DIR-868L');
-INSERT INTO GeoLocations (CountryName) VALUES ('Turkey'), ('USA'), ('Germany'), ('China');
