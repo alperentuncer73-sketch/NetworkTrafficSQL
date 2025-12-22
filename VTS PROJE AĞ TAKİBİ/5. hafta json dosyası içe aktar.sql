b0d6dd5bab0ff4434dc9b9e5a4cef689a8d44ded
Umut Can Kuş <umuut2024@gmail.com>
2025-12-22 22:58:57 +0300

    Update 5. hafta json dosyası içe aktar.sql

--- VTS PROJE AĞ TAKİBİ/5. hafta json dosyası içe aktar.sql
+++ VTS PROJE AĞ TAKİBİ/5. hafta json dosyası içe aktar.sql
@@ -1,10 +1,10 @@
 USE [Ag takibi proje];
 GO
---Önce, gelen veriyi olduðu gibi (ham haliyle) kabul edecek, kurallarý gevþek bir tablo oluþturalým.
--- Eðer varsa önce eskisini temizle
+--Ã–nce, gelen veriyi olduÄŸu gibi (ham haliyle) kabul edecek, kurallarÃ½ gevÅŸek bir tablo oluÅŸturalÄ±m
+-- EÃ°er varsa Ã¶nce eskisini temizle
 DROP TABLE IF EXISTS Staging_RawTraffic;
 
--- Ham veriyi tutacak geçici tablo
+-- Ham veriyi tutacak geÃ§ici tablo
 CREATE TABLE Staging_RawTraffic (
     StagingID INT IDENTITY(1,1),
     RawDeviceIP VARCHAR(50),      -- CSV/JSON'dan IP gelir (ID gelmez)
@@ -14,20 +14,20 @@ CREATE TABLE Staging_RawTraffic (
     RawStatus VARCHAR(20)
 );
 GO
---  Gelecek JSON dosyasýný simüle edelim. Burada OPENJSON komutunu kullanarak 
---JSON metnini tablo formatýna çevirip Staging tablosuna atýyoruz.
-PRINT '--- JSON Verisi Staging Tablosuna Alýnýyor ---';
+--  Gelecek JSON dosyasÄ±nÄ± simÃ¼le edelim. Burada OPENJSON komutunu kullanarak 
+--JSON metnini tablo formatÄ±na Ã§evirip Staging tablosuna atÃ½yoruz.
+PRINT ' JSON verisi staging tablosuna alÄ±nÄ±yor;
 
 DECLARE @json NVARCHAR(MAX);
 
--- Örnek JSON Verisi ( gelecek dosya bu formatta olacak)
+-- Ã–rnek JSON Verisi ( gelecek dosya bu formatta olacak)
 SET @json = '[
     {"source_ip":"192.168.1.1", "dest_ip":"8.8.8.8", "protocol":"DNS", "data_size":1.2, "status":"Allowed"},
     {"source_ip":"192.168.1.10", "dest_ip":"1.1.1.1", "protocol":"HTTPS", "data_size":15.5, "status":"Allowed"},
     {"source_ip":"192.168.1.99", "dest_ip":"10.0.0.5", "protocol":"TCP", "data_size":0.5, "status":"Blocked"}
 ]';
 
--- JSON'u parçala ve Staging tablosuna ekle
+-- JSON'u parÃ§ala ve Staging tablosuna ekle
 INSERT INTO Staging_RawTraffic (RawDeviceIP, RawDestIP, RawProtocol, RawDataSize, RawStatus)
 SELECT 
     source_ip, dest_ip, protocol, data_size, status
@@ -40,39 +40,40 @@ WITH (
     status VARCHAR(20)
 );
 
--- Kontrol edelim: Staging tablosunda veri var mý?
+-- Kontrol edelim: Staging tablosunda veri var mÃ½?
 SELECT * FROM Staging_RawTraffic;
 GO
---Þimdi Staging tablosundaki metinleri (DNS, 192.168.1.1) alýp, ana tablolarýmýzdaki 
---ID'lerle (ProtocolID, DeviceID) eþleþtirerek TrafficLogs tablosuna aktaracaðýz.
-PRINT '--- Veriler Dönüþtürülüp TrafficLogs Tablosuna Aktarýlýyor ---';
+--ÅŸimdi staging tablosundaki metinleri (DNS, 192.168.1.1) alÄ±p, ana tablolarÄ±mÄ±zdaki 
+--ID'lerle (ProtocolID, DeviceID) eÅŸleÅŸtirerek TrafficLogs tablosuna aktaracaÄŸÄ±z.
+PRINT '--- Veriler DÃ¶nÃ¼Ã¾tÃ¼rÃ¼lÃ¼p TrafficLogs Tablosuna AktarÃ½lÃ½yor ---';
 
 INSERT INTO TrafficLogs (
-    DeviceID,           -- ID'yi bulacaðýz
+    DeviceID,           -- ID'yi bulacaÃ°Ã½z
     SourceIPAddress, 
     DestinationIPAddress, 
-    DestinationPort,    -- Varsayýlan port atayacaðýz (veya protokolden bulacaðýz)
-    ProtocolID,         -- Metni ID'ye çevireceðiz
+    DestinationPort,    -- VarsayÄ±lan port atayacaÄŸÄ±z (veya protokolden bulacaÃ°Ã½z)
+    ProtocolID,         -- Metni ID'ye Ã§eviricez
     DataTransferredMB, 
     Status
 )
 SELECT 
-    D.DeviceID,                     -- 1. Devices tablosundan IP'ye göre ID bulduk
+    D.DeviceID,                     -- 1. Devices tablosundan IP'ye gÃ¶re ID bulduk
     S.RawDeviceIP,
     S.RawDestIP,
-    ISNULL(TP.DefaultPort, 0),      -- 2. Port bilgisini TrafficProtocols tablosundan çektik
-    TP.ProtocolID,                  -- 3. 'DNS' yazýsýný ProtocolID (örn: 5) yaptýk
+    ISNULL(TP.DefaultPort, 0),      -- 2. Port bilgisini TrafficProtocols tablosundan Ã§ektik
+    TP.ProtocolID,                  -- 3. 'DNS' yazÄ±sÄ±nÄ± ProtocolID (Ã¶rn: 5) yaptÄ±k
     S.RawDataSize,
     S.RawStatus
 FROM 
     Staging_RawTraffic AS S
--- Eþleþtirme (Mapping) Ýþlemleri:
+-- EÅŸleÅŸtirme (Mapping) yÃ¶ntemleri:
 LEFT JOIN Devices AS D ON S.RawDeviceIP = D.DeviceIP
 LEFT JOIN TrafficProtocols AS TP ON S.RawProtocol = TP.ProtocolName;
 
--- Sonucu Görelim
+-- Sonucu GÃ¶relim
 SELECT * FROM TrafficLogs ORDER BY LogID DESC;
 GO
---Ýþlem bittikten sonra geçici tabloyu temizleyebiliriz
--- Staging tablosunu boþalt (Sonraki yükleme için hazýrla)
+--iÅŸlem bittikten sonra geÃ§ici tabloyu temizleyebiliriz
+-- Staging tablosunu boÅŸalt (Sonraki yÃ¼kleme iÃ§in hazÄ±rla)
+
 TRUNCATE TABLE Staging_RawTraffic;
