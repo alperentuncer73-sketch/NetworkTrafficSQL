73ab66c5cc9dff4fd58643f76e4414dc7dd1d10c
Umut Can Kuş <umuut2024@gmail.com>
2025-12-22 23:13:31 +0300

    Update 9. hafta trigger test2.sql

--- VTS PROJE AĞ TAKİBİ/9. hafta trigger test2.sql
+++ VTS PROJE AĞ TAKİBİ/9. hafta trigger test2.sql
@@ -1,6 +1,7 @@
--- Test Verisi Ekleme (Bu veri 2500 MB olduðu için Critical uyarýsý vermeli)
+-- Test Verisi Ekleme (Bu veri 2500 MB olduÃ°u iÃ§in Critical uyarÄ±sÄ± vermeli)
 INSERT INTO TrafficLogs (DeviceID, UserID, SourceIPAddress, DestinationIPAddress, DestinationPort, ProtocolID, DataTransferredMB, PacketCount, Status, EventTimestamp)
 VALUES (1, 1, '192.168.1.99', '8.8.8.8', 443, 2, 2500.00, 10000, 'Allowed', GETDATE());
 
 
 SELECT * FROM SecurityAlerts ORDER BY AlertID DESC;
+
